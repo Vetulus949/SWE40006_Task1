2f92e5166bbac800b9253fac377540403a1667a3
Dom <34756785+Vetulus949@users.noreply.github.com>
2025-08-14 16:53:38 +1000

    Adding Code to the Repo

---
+++ task1_1/ClassLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private bool bTest = false;
+        public void test_method()
+        { }
+        public int add(int val1, int val2)
+        {
+            return val1 + val2;
+        }
+        public bool extra
+        {
+            get
+            {
+                return bTest;
+            }
+            set
+            {
+                bTest = extra;
+            }
+        }
+    }
+}
+
---
+++ task1_1/task1_1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+namespace task1_1
+{
+    public partial class Form1 : Form
+    {
+        private int number = 0;
+        private Random random = new Random();
+        private Class1 class1 = new Class1();
+        //
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int guess = int.Parse(textBox1.Text);
+            int difference = class1.add(guess, number);
+            label3.Text = $"sum of guess and the secret number is {difference}";
+            if (guess == number) {
+                label2.Text = "you guessed the number!";
+            } else {
+                label2.Text = "you guessed wrong :(";
+            }
+        }
+        //
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            number = random.Next(1, 10);
+            label2.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            number = random.Next(1, 10);
+            label2.Text = "";
+            label3.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label2.Text = "";
+        }
+    }
+}
